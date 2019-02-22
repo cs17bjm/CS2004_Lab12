7877b2cb5a876ba32c4cd0987a0a5e7c7baf9c37
Brandon <1624079@brunel.ac.uk>
2019-02-22 19:49:24 +0000

    Complete

--- src/Lab12.java
+++ src/Lab12.java
@@ -1,4 +1,30 @@
+public class Lab12 
+{
+	public static void main(String args[])
+	{
+		//Run 10 repeats
+		RunManyTimes(3);
 
-public class Lab12 {
+	}
+	public static void RunManyTimes (int x) {
+		for(int i=0;i<x;++i)
+		{
+			//Reset the fitness count
+			OneMaxChrome.ClearFC();
+			//The following parameters are not very good!
+			//These are the ones you should try and optimise!
+			int n = 1000;	
+			int popsize = 100;
+			double mrate = (double) 1/n;
+			double crate = 0.40;		
+			//You will not need to change the following
+			SimpleGeneticAlgorithm ga = new SimpleGeneticAlgorithm(popsize,10,n,mrate,crate);
+			//Run the GA for 10,000 function calls
+			double f = ga.RunSGA(10000,false,i+1).GetFitness();
+			
+			System.out.println(f);
+		}
+	}
+	
 
 }
